f3623da869ec6d40a097ebdc763ea6b369865d46
Vitor Lopes de Souza <vitorlopesdesouza@gmail.com>
2020-08-19 10:45:58 -0300

    first commit

---
+++ src/MentionListItem/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, View, TouchableOpacity, Image } from "react-native";
+
+// Styles
+import styles from "./MentionListItemStyles";
+
+import Avatar from "../Avatar";
+
+export class MentionListItem extends React.PureComponent {
+  static propTypes = {
+    item: PropTypes.object,
+    onSuggestionTap: PropTypes.func,
+    editorStyles: PropTypes.object
+  };
+
+  onSuggestionTap = (user, hidePanel) => {
+    this.props.onSuggestionTap(user);
+  };
+
+  render() {
+    const { item: user, index, editorStyles } = this.props;
+    return (
+      <View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={[styles.suggestionItem, editorStyles.mentionListItemWrapper]}
+          onPress={() => this.onSuggestionTap(user)}
+        >
+          <Image
+            source={{ uri: user.avatar}}
+            style={styles.avatar}
+          />
+
+          <View style={[styles.text, editorStyles.mentionListItemTextWrapper]}>
+            <Text style={[styles.title, editorStyles.mentionListItemTitle]}>
+              {user.displayName}
+            </Text>
+            <Text
+              style={[styles.username, editorStyles.mentionListItemUsername]}
+            >
+              @{user.username}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default MentionListItem;
